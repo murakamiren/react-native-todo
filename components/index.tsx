2e1b4edd458ad709470893190c9eaefe6d008879
renmurakami <autoeyy@gmail.com>
2022-04-17 11:32:55 +0900

    initial commit

---
+++ components/index.tsx
@@ -0,0 +1,12 @@
+import { VFC } from "react";
+import { Text, View } from "react-native";
+
+const Index: VFC = () => {
+	return (
+		<View>
+			<Text>a</Text>
+		</View>
+	);
+};
+
+export default Index;
